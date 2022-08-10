04cc8bc2b5c1b74ed137ba39aa02a4ad1c72e3b1
keinuma <nununu.mono@gmail.com>
2022-08-10 17:42:04 +0900

    Add ECS service

--- server/ecs.tf
+++ server/ecs.tf
@@ -1,5 +1,92 @@
+######################
+# IAM Role
+######################
+data "aws_iam_policy_document" "ecs_task_role_assume_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role" "api_ecs_task_execution_role" {
+  name               = "${var.product_name}-ApiTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_role_assume_policy.json
+  tags = {
+    "environment" = var.environment
+    "product"     = var.product_name
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.api_ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "api_ecs_task_role" {
+  name               = "${var.product_name}-ApiTaskRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_role_assume_policy.json
+  tags = {
+    "environment" = var.environment
+    "product"     = var.product_name
+  }
+}
+
+######################
+# ECS
+######################
 resource "aws_ecs_cluster" "api_cluster" {
   name = "${var.product_name}-api"
+
+  tags = {
+    "environment" = var.environment
+    "product"     = var.product_name
+  }
+}
+
+resource "aws_ecs_service" "api_service" {
+  name        = "${var.product_name}-api"
+  cluster     = aws_ecs_cluster.api_cluster.id
+  launch_type = "FARGATE"
+
+  tags = {
+    "environment" = var.environment
+    "product"     = var.product_name
+  }
+}
+
+resource "aws_ecs_task_definition" "api_task_definition" {
+  family                   = "${var.product_name}-api"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  task_role_arn            = aws_iam_role.api_ecs_task_role.arn
+  execution_role_arn       = aws_iam_role.api_ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      "name" : "app",
+      "image" : "${aws_ecr_repository.api.repository_url}",
+      "essential" : true,
+      "portMappings" : [
+        {
+          "containerPort" : 3000,
+          "hostPort" : 3000
+        }
+      ],
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group" : "/ecs/app",
+          "awslogs-region" : "ap-northeast-1",
+          "awslogs-stream-prefix" : "${var.product_name}-api"
+        }
+      }
+    }
+  ])
 
   tags = {
     "environment" = var.environment
--- server/outputs.tf
+++ server/outputs.tf
@@ -1,3 +1,23 @@
 output "api_ecr_repo_arn" {
   value = aws_ecr_repository.api.arn
 }
+
+output "api_ecs_cluster_name" {
+  value = aws_ecs_cluster.api_ecs_cluster.name
+}
+
+output "api_ecs_service_name" {
+  value = aws_ecs_service.api_ecs_service.name
+}
+
+output "api_ecs_service_arn" {
+  value = aws_ecs_service.api_ecs_service.id
+}
+
+output "api_ecs_task_role_arn" {
+  value = aws_iam_role.api_ecs_task_role.arn
+}
+
+output "api_ecs_task_execution_role_arn" {
+  value = aws_iam_role.api_ecs_task_execution_role.arn
+}
