35eab462152fb2a0512fecdeb86451b20bac2da1
keinuma <nununu.mono@gmail.com>
2022-08-11 15:52:02 +0900

    Fix option of ACM

--- server/acm.tf
+++ server/acm.tf
@@ -14,14 +14,22 @@ resource "aws_acm_certificate" "api_acm_certifacate" {
 
 resource "aws_acm_certificate_validation" "api_acm_certificate_validation" {
   certificate_arn         = aws_acm_certificate.api_acm_certifacate.arn
-  validation_record_fqdns = [aws_route53_record.api.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.api_cert_verification_r53_record : record.fqdn]
 }
 
 resource "aws_route53_record" "api_cert_verification_r53_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.api_acm_certifacate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   allow_overwrite = true
-  name            = aws_acm_certificate.api_acm_certifacate.domain_validation_options.value.name
-  records         = [aws_acm_certificate.api_acm_certifacate.domain_validation_options.value.record]
+  name            = each.value.name
+  records         = [each.value.record]
   ttl             = 60
-  type            = aws_acm_certificate.api_acm_certifacate.domain_validation_options.value.type
+  type            = each.value.type
   zone_id         = aws_route53_record.api.zone_id
 }
